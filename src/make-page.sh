eb0b490b7941a5915a43cd5b2bc499d8280dfd8f
Gergely Nagy <algernon@balabit.hu>
2012-04-02 19:57:03 +0200

    Add the logo to the generated pages.
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- src/make-page.sh
+++ src/make-page.sh
@@ -5,4 +5,5 @@ pandoc -t html5 libumberlog/README.rst >README.html
 pandoc -t html5 libumberlog/lib/umberlog.rst >umberlog.3.html
 sed '/@README@/r README.html' src/index.html.in | sed -e 's/<!-- @home@ -->/class="active"/' -e 's/<!-- @api@ -->//' >index.html
 sed '/@README@/r umberlog.3.html' src/index.html.in | sed -e 's/<!-- @api@ -->/class="active"/' -e 's/<!-- @home@ -->//' >umberlog.html
+cp libumberlog/data/umberlog-logo-full.png umberlog-logo.png
 rm -rf libumberlog README.html umberlog.3.html
