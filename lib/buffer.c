4158df7874fe07ff731d756c9742188ce4d6e8c2
Miloslav Trmač <mitr@redhat.com>
2012-06-21 13:01:07 +0200

    Fix 1-byte heap overflow in ul_buffer_finalize
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- lib/buffer.c
+++ lib/buffer.c
@@ -225,10 +225,14 @@ char *
 ul_buffer_finalize (ul_buffer_t *buffer)
 {
   if (buffer->msg[buffer->len - 1] == ',')
-    buffer->msg[buffer->len - 1] = '}';
-  else
     {
       if (!_ul_buffer_ensure_size (buffer, buffer->len + 1))
+        return NULL;
+      buffer->msg[buffer->len - 1] = '}';
+    }
+  else
+    {
+      if (!_ul_buffer_ensure_size (buffer, buffer->len + 2))
         return NULL;
       buffer->msg[buffer->len++] = '}';
     }
